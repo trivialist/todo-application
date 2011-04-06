dc96e008db79ce1610535039474d9753496399d6
sven.skrabal@konzept-e.de <sven.skrabal@konzept-e.de@c14708ac-2b4f-11df-8e42-19cc402ff320>
2011-04-06 13:35:37 +0000

    checkin of migration project

---
+++ todoRelationMigrate/src/todorelationsmigrate/DB_Mitarbeiter_Connect.java
@@ -0,0 +1,60 @@
+/*
+ * DB_Mitarbeiter_Connect.java
+ *
+ * Created on 22. November 2006, 13:27
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+package todorelationsmigrate;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hertel
+ */
+public class DB_Mitarbeiter_Connect
+{
+
+	private static Connection con = null;
+
+	/** Creates a new instance of DB_Mitarbeiter_Connect */
+	public DB_Mitarbeiter_Connect()
+	{
+	}
+
+	public static void openDB()
+	{
+		try
+		{
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver").newInstance();
+
+			String url = "jdbc:odbc:personal_db";
+			con = DriverManager.getConnection(url);
+		} catch (Exception ex)
+		{
+			Logger.getLogger(DB_Mitarbeiter_Connect.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+	}
+
+	public static void closeDB(Connection con)
+	{
+		try
+		{
+			con.close();
+		} catch (Exception ex)
+		{
+			Logger.getLogger(DB_Mitarbeiter_Connect.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public static Connection getCon()
+	{
+		return con;
+	}
+}
